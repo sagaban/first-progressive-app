2f824d4ee705ba5170123d0ada6e69eebd6cf9c2
Santiago Bandiera <sagaban@hotmail.com>
2019-07-14 19:01:56 -0300

    Add dotenv and gitignore

--- server.js
+++ server.js
@@ -21,6 +21,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+require('dotenv').config()
 
 // CODELAB: Change this to add a delay (ms) before the server responds.
 const FORECAST_DELAY = 0;
